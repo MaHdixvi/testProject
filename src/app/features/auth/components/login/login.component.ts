0be60cab0b200d03c31738c1b72c879052ecb8fc
mahdi <131790640+MaHdixvi@users.noreply.github.com>
2025-09-15 13:08:40 +0330

    taktom said commit kon

--- src/app/features/auth/components/login/login.component.ts
+++ src/app/features/auth/components/login/login.component.ts
@@ -1,11 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone:true,
+  imports:[FormsModule,CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  formlogin={
+    username:'',
+    password:''
+  }
+  onSubmit(form: NgForm) {
+    console.log
+    if (form.valid) {
+      const username = form.value.username;
+      const password = form.value.password;
+      // Handle login logic here
+      console.log('Logging in with:', username, password);
+    }
+  }
 }
